a004767439560aeaf8d15fa9bc1f0f89ba36ed67
Beth Telford <mrsbethtelford@gmail.com>
2017-06-27 15:01:56 -0600

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const bodPars = require('body-parser');
+const express = require('express');
 
+var app = express();
+app.use(express.static('assets'));
+app.use(bodPars.json());
+
+var messages = [];
+
+app.get('/messages', function(req, res, next) {
+    res.status(200).json({ messages: messages });
+})
+
+app.post('/messages', function(req, res, next) {
+    messages.push({ message: req.body.message, time: new Date() });
+    res.status(200).json({ messages: messages });
+})
+
+
+
+
+
+
+app.listen(3000);
